e0604b6c7ebc758f190676782929d61c94fd59de
frbarbre <fr.barbre@gmail.com>
2023-10-06 02:36:40 +0200

    played a bit

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,15 @@
 export default async function Home() {
   async function fetchGameData(
-    params = 'games',
-    body = 'fields name,total_rating,rating_count,screenshots,slug; where id = 7346;'
+    params = "games",
+    body = "fields name,total_rating,rating_count,screenshots,slug; where id = 7346;"
   ) {
     try {
       const response = await fetch(`https://api.igdb.com/v4/${params}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          Accept: 'application/json',
-          'Client-ID': 'sqrrpikp954lcrzih7ld9g2ex4eb5h',
-          Authorization: 'Bearer quvnddpc62z5nxinyzoh5o2wwt1qst',
+          Accept: "application/json",
+          "Client-ID": process.env.IGDB_CLIENT_ID,
+          Authorization: `Bearer ${process.env.IGDB_TOKEN}`,
         },
         body: body,
       });
@@ -21,26 +21,26 @@ export default async function Home() {
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error(error);
+      console.error(error + " " + params);
     }
   }
-
-  const gameid = 148241;
-
-  const game = await fetchGameData(
-    'games',
-    `fields name,total_rating,rating_count,screenshots,slug; where id = ${gameid};`
-  );
-  // console.log(game);
+  const searchParams = "Phasmophobia";
 
   const search = await fetchGameData(
-    'search',
-    `fields *; search "lies of p"; limit 100;`
+    "games",
+    `fields *; search "${searchParams}"; limit 100; where version_parent = null;`
   );
-  console.log(search);
+  // console.log(search);
+  const gameid = "20196"
+
+  const game = await fetchGameData(
+    "games",
+    `fields name,aggregated_rating,rating, version_parent; where version_parent = null & rating_count > 50 & parent_game = null & aggregated_rating != null & aggregated_rating_count > 5; sort aggregated_rating desc; limit 20;`
+  );
+  console.log(game);
 
   const gameScreenshots = await fetchGameData(
-    'screenshots',
+    "covers",
     `fields *; where game = ${gameid};`
   );
 
@@ -50,7 +50,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {gameScreenshots.map((screenshot) => (
         <img
-          src={`https://images.igdb.com/igdb/image/upload/t_1080p/${screenshot.image_id}.jpg`}
+          src={`https://images.igdb.com/igdb/image/upload/t_1080p/${screenshot.image_id}.png`}
           alt=""
           width={screenshot.width}
           height={screenshot.height}
